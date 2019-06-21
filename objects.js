ccc8ba51b712fb2bf64fb02f7258efd66a89dfbc
Paul Yun <p.yun1994@gmail.com>
2019-06-21 21:23:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,13 @@ let playlist = {
   BiggieSmalls : 'Juicy',
   KendrickLamar : 'Humble'
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist;
+}
